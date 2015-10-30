80237e3bd090078f928d7a55a0d367d35958a26a
felixzhuologist <felix.czhu@gmail.com>
2015-10-30 23:30:24 +0100

    initial commit

---
+++ my_hash.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""This module contains the MyHash class."""
+
+from numbers import Number
+#TODO run linter/make sure style is fine
+#TODO peer review (Rohan)?
+#TODO choose better default value?
+class MyHash(object):
+    """Custom hash map which handles collisions using Cuckoo Hashing."""
+    def __init__(self, size):
+        """Initialize hash table of given size."""
+        self._assert_valid_size(size)
+        self.size = size
+        self.nitems = 0
+        self.array = [None] * int(size)
+
+    #TODO: should be able to re set a value with same key
+    def set(self, key, value):
+        """Set key and value and return True on success, False on failure."""
+        self._assert_valid_key(key)
+        index = self._get_hash(key)
+        if self._is_full() or self._already_defined(index):
+            return False
+        else: # found a spot for the value
+            self.array[index] = value
+            self.nitems += 1
+            return True
+
+    def get(self, key):
+        """
+        Return value associated with input key if it has been set, None
+        otherwise.
+        """
+        index = self._get_hash(key)
+        if self._already_defined(index):
+            return self.array[index]
+        else: # no value was set for given key
+            return None
+
+    #TODO: maybe sacrifice conciseness for readability here?
+    def delete(self, key):
+        """
+        Delete key from hash map and return the associated value on success,
+        or None on failure.
+        """
+        index = self._get_hash(key)
+        value, self.array[index] = self.array[index], None
+        self.nitems = self.nitems - 1 if value is not None else self.nitems
+        return value
+
+    def load(self):
+        """Return the current load of the hash map."""
+        return float(self.nitems) / self.size
+
+    def _already_defined(self, index):
+        """Return True if hash map is already defined at given index, else False."""
+        return self.array[index] is not None
+
+    @classmethod
+    def _assert_valid_key(cls, key):
+        """Raise TypeError if the input is not a valid key."""
+        if not isinstance(key, str):
+            raise TypeError("Keys must be strings.")
+
+    def _assert_valid_size(self, size):
+        """Raise appropriate error if input is not a natural number."""
+        if not isinstance(size, Number):
+            raise TypeError("Size must be a valid integer")
+        elif size < 0 or int(size) != size:
+            raise ValueError("Size must be non negative")
+
+    def _get_hash(self, string):
+        """Return the hash of the input string."""
+        return hash(string) % self.size
+
+    def _is_full(self):
+        """Return True if the hash map is full, False otherwise."""
+        return self.nitems == self.size
+
+    #TODO: better hash function?
+    def _get_hashes(self, string):
+        return [self._get_hash(string + str(i)) for i in range(3)]
+
---
+++ test_my_hash.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+import unittest
+from my_hash import MyHash
+
+
+class TestMyHashConstruction(unittest.TestCase):
+	def test_construction_valid_arg(self):
+		self.assertEqual(3, MyHash(3.0).size)
+		self.assertEqual(200, MyHash(200).size)
+		self.assertEqual(0, MyHash(0).size)
+		self.assertEqual(1000, MyHash(1000).size)
+
+	def test_construction_bad_type(self):
+		with self.assertRaises(TypeError):
+			MyHash('string')
+
+	def test_construction_bad_value(self):
+		with self.assertRaises(ValueError): 
+			MyHash(-1)
+		with self.assertRaises(ValueError): 
+			MyHash(-1.0)
+		with self.assertRaises(ValueError):
+			MyHash(2.2)
+
+
+class TestMyHashSetGet(unittest.TestCase):
+	def setUp(self):
+		self.small_hash_map = MyHash(20)
+		self.big_hash_map = MyHash(1000)
+		self.alice_text = open('alice.txt', 'r').read()
+
+	def test_set_get_full(self):
+		bulk_set_results = self._bulk_set(self.small_hash_map, 20)
+		self.assertTrue(all(bulk_set_results))
+		bulk_get_results = self._bulk_get(self.small_hash_map, 20)
+		self.assertEqual(range(20), bulk_get_results)
+
+	def test_good_keys(self):
+		self.assertTrue(self.big_hash_map.set("", "somevalue"))
+		self.assertTrue(self.big_hash_map.set("somekey", (1, 2, 3)))
+		self.assertTrue(self.big_hash_map.set("ʕ•ᴥ•ʔ", "(•ω•)"))
+		self.assertTrue(self.big_hash_map.set(self.alice_text, 'alice'))
+
+		self.assertEqual(self.big_hash_map.get(""), "somevalue")
+		self.assertEqual(self.big_hash_map.get("somekey"), (1, 2, 3))
+		self.assertEqual(self.big_hash_map.get("ʕ•ᴥ•ʔ"), "(•ω•)")
+		self.assertEqual(self.big_hash_map.get(self.alice_text), 'alice')
+
+	def test_set_by_assignment(self):
+		value = [1, 2, 3]
+		self.big_hash_map.set("list", value)
+		value.append(4)
+		self.assertEqual(value, self.big_hash_map.get("list"))
+
+	def test_bad_keys(self):
+		pass
+		#not string key, full hash_map
+
+	def _bulk_set(self, hash_map, nitems):
+		return [hash_map.set("key" + str(i), i) for i in range(1, nitems)]
+
+	def _bulk_get(self, hash_map, nitems):
+		return [hash_map.get("key" + str(i) for i in range(1, nitems))]
+
+
+class TestMyHashDelete(unittest.TestCase):
+	def setUp(self):
+		self.hash_map = MyHash(50)
+
+	def test_delete_existing(self):
+		self.hash_map.set("somekey", 20)
+		self.assertEqual(self.hash_map.delete("somekey"), 20)
+		self.assertIsNone(self.hash_map.get("somekey"))
+
+	def test_delete_nonexisting(self):
+		self.assertIsNone(self.hash_map.delete("somekey"))
+
+
+class TestMyHashLoad(unittest.TestCase):
+	def setUp(self):
+		self.hash_map = MyHash(5)
+
+	def test_load(self):
+		self.assertEqual(self.hash_map.load(), 0)
+		self.hash_map.set('key', 0)
+		self.assertAlmostEqual(self.hash_map.load(), 0.2)
+		self.hash_map.set('otherkey', 0)
+		self.assertAlmostEqual(self.hash_map.load(), 0.4)
+		self.hash_map.delete('key')
+		self.assertAlmostEqual(self.hash_map.load(), 0.2)
+		self.hash_map.delete('otherkey')
+		self.assertEqual(self.hash_map.load(), 0)
+
+# if __name__ == '__main__':
+# 	unittest.main()
